5db394d815f9d95a28ce8f4f112dc842bbe29e7f
Guilherme Tonioli <guilhermestonioli@gmail.com>
2019-11-19 23:15:53 -0300

    Remove console.log

--- ui/src/components/Room/Chat/Entry/Roll/Roll.js
+++ ui/src/components/Room/Chat/Entry/Roll/Roll.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 
 class Roll extends Component {
    render() {
-      console.log(this.props.data);
       return (
          <div className="roll">
             <b>{this.props.input}</b>: {this.props.result.result}
